17c7796077b330f1a0422d86e13a0810d6c4a17e
Lina <lina@bhatia.org>
2022-08-18 20:24:08 -0500

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Lina Bhatia on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
